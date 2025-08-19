60f5d5c9914f59a56c989ddb13a76042b49751bd
DanielCachro <danielcachro@gmail.com>
2025-08-19 17:21:50 +0200

    fix(PrimaryButton): improve PrimaryButton rendering and add new props
    - Fix issue where plain text `content` rendered incorrectly
    - Add new props: `className`, `wrapperClassName`, `shadowClassName` for greater styling flexibility
    - Replace `peer-focus` with `peer-focus-visible` for more accurate focus handling
    - Update hover and focus-visible effects in dark mode

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -6,21 +6,41 @@ import {JSX} from 'react'
 
 interface Props {
 	content: string | JSX.Element
+	className?: string
+	wrapperClassName?: string
+	shadowClassName?: string
 }
 
-export default function PrimaryButton({content}: Props) {
+export default function PrimaryButton({content, className, wrapperClassName, shadowClassName}: Props) {
 	return (
-		<div className={clsx('relative inline-block', typeof content === 'string' ? 'px-16 py-12' : 'h-24 w-24')}>
+		<div
+			className={clsx(
+				'relative inline-block',
+				{
+					'h-24 w-24': typeof content !== 'string',
+				},
+				wrapperClassName,
+			)}>
 			<motion.button
 				whileTap={{
 					y: 3,
 					boxShadow: 'none',
 					transition: {duration: 0.075},
 				}}
-				className='relative z-10 h-full w-full rounded-sm bg-primary-500 font-bold text-primary-100 transition-colors duration-50 hover:cursor-pointer hover:bg-primary-400 focus-visible:bg-primary-400 focus-visible:outline-2 focus-visible:outline-primary-200 dark:bg-primary-600 dark:text-primary-200 dark:hover:bg-primary-500 dark:focus-visible:outline-primary-500'>
+				className={clsx(
+					'peer relative z-10 h-full w-full rounded-sm bg-primary-500 font-bold text-primary-100 transition-colors duration-50 hover:cursor-pointer hover:bg-primary-400 focus-visible:bg-primary-400 focus-visible:outline-2 focus-visible:outline-primary-200 dark:bg-primary-600 dark:text-primary-200 dark:hover:bg-primary-500 focus-visible:dark:bg-primary-500 focus-visible:dark:outline-primary-300',
+					{
+						'px-16 py-12': typeof content === 'string',
+					},
+					className,
+				)}>
 				{content}
 			</motion.button>
-			<div className='absolute top-[3px] left-0 z-0 h-full w-full rounded-sm bg-primary-700 transition-colors duration-50 peer-hover:bg-primary-600 peer-focus:bg-primary-600 dark:bg-primary-800 dark:peer-hover:bg-primary-700'></div>
+			<div
+				className={clsx(
+					'absolute top-[3px] left-0 z-0 h-full w-full rounded-sm bg-primary-700 transition-colors duration-50 peer-hover:bg-primary-600 peer-focus-visible:bg-primary-600 dark:bg-primary-800 dark:peer-hover:bg-primary-700',
+					shadowClassName,
+				)}></div>
 		</div>
 	)
 }
