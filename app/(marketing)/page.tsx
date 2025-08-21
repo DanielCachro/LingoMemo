0b4d09dbdc0461a9779bbe2ed17df00475784523
DanielCachro <danielcachro@gmail.com>
2025-08-21 19:09:44 +0200

    refactor: adjust spacing on Landing page for improved visual hierarchy
    - Modify spacing between elements
    - Enhance readability and overall layout balance

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -7,11 +7,13 @@ export default function LandingPage() {
 	return (
 		<section className='mx-32 flex min-h-dvh flex-col items-center justify-center gap-32 text-center'>
 			<Image src='/cats/CatSmile.svg' alt='Brand cat smiling' width={128} height={120} className='w-128' />
-			<div className='max-w-384 space-y-24'>
-				<h1 className='text-2xl font-bold'>
-					A smarter way to learn vocabulary<span className='text-primary-500 dark:text-primary-600'>.</span>
-				</h1>
-				<p>Create your own cards, track your progress, and never forget the words that matter.</p>
+			<div className='max-w-384 space-y-32'>
+				<div className='space-y-12'>
+					<h1 className='text-2xl font-bold'>
+						A smarter way to learn vocabulary<span className='text-primary-500 dark:text-primary-600'>.</span>
+					</h1>
+					<p>Create your own cards, track your progress, and never forget the words that matter.</p>
+				</div>
 				<PrimaryButton>
 					<FontAwesomeIcon icon={faGoogle} size='lg' className='mr-12' />
 					Sign in with Google
