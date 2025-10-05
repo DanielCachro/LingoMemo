96e17e962340a35fd465eebebc6f029c471dbbae
DanielCachro <danielcachro@gmail.com>
2025-10-05 15:54:54 +0200

    feat(home): add dynamic chart message in WeekChartSection based on days with completed flashcards

--- app/(protected)/home/_sections/WeekChartSection/index.tsx
+++ app/(protected)/home/_sections/WeekChartSection/index.tsx
@@ -5,11 +5,29 @@ import Chart from './Chart'
 export default async function WeekChart() {
 	const last7Days = await getLast7DaysCompletionCount()
 
+	let activeDaysThisWeek = 0
+	for (const day of last7Days) {
+		if (day.cardsCompleted > 0) {
+			activeDaysThisWeek++
+		}
+	}
+
+	let chartMessage = ''
+	if (activeDaysThisWeek === 0) {
+		chartMessage = 'No cards completed this week.'
+	} else if (activeDaysThisWeek <= 1) {
+		chartMessage = 'Great start! Keep going!'
+	} else if (activeDaysThisWeek <= 3) {
+		chartMessage = 'Doing well this week!'
+	} else {
+		chartMessage = 'Whoa, look at your week!'
+	}
+
 	return (
 		<section className='flex-col items-center py-48 min-[26.8rem]:flex'>
 			<div>
 				<FadeInChildren delay={0.7} delayStep={0} duration={0.3}>
-					<p className='ml-24 pb-8 text-lg font-bold sm:m-0'>Whoa, look at your week!</p>
+					<p className='ml-24 pb-8 text-lg font-bold sm:m-0'>{chartMessage}</p>
 				</FadeInChildren>
 				<div className='no-scrollbar overflow-x-scroll pl-24 sm:p-0' style={{direction: 'rtl'}}>
 					<Chart last7Days={last7Days} />
