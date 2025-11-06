249bc896610fc27cea2c74eeb1f9b793199e4a78
DanielCachro <danielcachro@gmail.com>
2025-11-06 16:28:22 +0100

    fix: prevent `ReferenceError: window is not defined` build error
    - resolve `ReferenceError: window is not defined` during the build process
    - dynamically import Modal component using `next/dynamic` with `ssr: false`
    - ensures the component only renders on the client-side where `window` and `document` are available
    - use this approach because the component relies heavily on client-side `window` and `document` objects

--- app/@modal/(.)preferences/layout.tsx
+++ app/@modal/(.)preferences/layout.tsx
@@ -1,7 +1,9 @@
+'use client'
 import Breadcrumbs from '@/components/Breadcrumbs'
-import Modal from '../_components/Modal'
+import dynamic from 'next/dynamic'
+const Modal = dynamic(() => import('../_components/Modal'), {ssr: false})
 
-export default async function PreferencesLayout({children}: {children: React.ReactNode}) {
+export default function PreferencesLayout({children}: {children: React.ReactNode}) {
 	return (
 		<Modal header='mobile' heading='Preferences' className='sm:w-640'>
 			<div className='flex h-full flex-col'>
