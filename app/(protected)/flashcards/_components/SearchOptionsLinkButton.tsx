f9c39b0290fc9727be43ce5fe2ab2f33b39ea77f
DanielCachro <danielcachro@gmail.com>
2025-11-08 16:53:02 +0100

    fix: use :focus-visible for correct keyboard focus styles
    - replace `:focus-within` with `:focus-visible` in `flashcards/_components/SearchOptionsLinkButton.tsx`
    - replace `:focus-within` with `:focus-visible` in `components/Form/Checkbox.tsx`
    - corrects behavior to ensure focus styles apply specifically during keyboard navigation

--- app/(protected)/flashcards/_components/SearchOptionsLinkButton.tsx
+++ app/(protected)/flashcards/_components/SearchOptionsLinkButton.tsx
@@ -15,7 +15,7 @@ export default function SearchOptionsLinkButton({icon, href, isActive = false}: 
 		<Link
 			href={href}
 			className={cn(
-				'group flex aspect-square w-[3.25rem] items-center justify-center rounded-sm border-2 border-background-300 bg-background-50 transition-colors duration-150 focus-within:border-background-400 hover:border-background-400 focus:outline-none dark:border-background-700 dark:bg-background-900 dark:focus-within:border-background-600 dark:hover:border-background-600',
+				'group flex aspect-square w-[3.25rem] items-center justify-center rounded-sm border-2 border-background-300 bg-background-50 transition-colors duration-150 hover:border-background-400 focus:outline-none focus-visible:border-background-400 dark:border-background-700 dark:bg-background-900 dark:hover:border-background-600 dark:focus-visible:border-background-600',
 				{
 					'border-primary-500 bg-primary-100 hover:border-primary-400 dark:border-primary-500 dark:bg-primary-700 dark:hover:border-primary-400':
 						isActive,
