92531373171d6200a3e30ec8ad5c1cd92a832681
DanielCachro <danielcachro@gmail.com>
2025-11-19 14:41:33 +0100

    feat: rename TagField to TagInput and enhance functionality
    - rename `TagField` component to `TagInput` for consistency
    - add support for highlighting errors on specific inputs
    - implement `onTagRemove` callback

--- components/Form/TagInput.tsx
+++ components/Form/TagInput.tsx
@@ -3,22 +3,33 @@ import {cn} from '@/lib/utils'
 import {faPlus, faXmark} from '@fortawesome/free-solid-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import {Input as HeadlessInput, InputProps} from '@headlessui/react'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import ErrorMessage from './ErrorMessage'
 
 interface Props extends InputProps {
 	name: string
 	className?: string
 	initialTags?: string[]
-	error?: boolean
-	errorMessage?: string
-	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+	onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+	onTagRemove?: (index: number) => void
+	errorInTag?: {
+		index: number
+		message?: string
+	}[]
 }
 
-export default function TagField({error, errorMessage, className, name, initialTags = [], onChange, ...props}: Props) {
+export default function TagInput({
+	className,
+	name,
+	initialTags = [],
+	onChange,
+	onTagRemove,
+	errorInTag,
+	...props
+}: Props) {
 	const [inputValue, setInputValue] = useState('')
 	const [tags, setTags] = useState<string[]>(Array.from(new Set(initialTags)))
-
+	const [errors, setErrors] = useState(errorInTag ?? [])
 	function handleAddTag() {
 		const trimmed = inputValue.trim()
 		if (!trimmed) return
@@ -27,9 +38,18 @@ export default function TagField({error, errorMessage, className, name, initialT
 		setInputValue('')
 	}
 
-	function handleRemoveTag(tagToRemove: string) {
-		const updated = tags.filter(tag => tag !== tagToRemove)
+	function handleRemoveTag(index: number) {
+		const updated = tags.filter((_, i) => i !== index)
+		const updatedErrors = errors
+			.filter(error => error.index !== index)
+			.map(error => ({
+				...error,
+				index: error.index > index ? error.index - 1 : error.index,
+			}))
+
 		setTags(updated)
+		setErrors(updatedErrors)
+		onTagRemove?.(index)
 	}
 
 	function handleEnter(e: React.KeyboardEvent<HTMLInputElement>) {
@@ -39,22 +59,24 @@ export default function TagField({error, errorMessage, className, name, initialT
 		}
 	}
 
+	useEffect(() => {
+		setErrors(errorInTag ?? [])
+	}, [errorInTag])
+
 	return (
 		<div>
 			<div className='space-y-16'>
 				<input type='hidden' name={name} value={JSON.stringify(tags)} />
 
 				<div className='flex flex-wrap gap-8'>
-					{tags.map(tag => (
-						<Tag key={tag} tag={tag} onRemove={handleRemoveTag} />
-					))}
+					{tags.map((tag, index) => {
+						const tagError = errors.find(error => error.index === index)
+						return <Tag key={tag} tag={tag} onRemove={() => handleRemoveTag(index)} error={!!tagError} />
+					})}
 				</div>
 				<div
 					className={cn(
 						'flex space-x-12 rounded-sm border-2 border-background-300 bg-background-50 px-16 py-16 text-base placeholder-background-400 placeholder:font-medium focus-within:border-background-400 focus:outline-none dark:border-background-700 dark:bg-background-900 dark:focus-within:border-background-600',
-						{
-							'border-error-400 dark:border-error-700': error,
-						},
 						className,
 					)}>
 					<HeadlessInput
@@ -75,21 +97,35 @@ export default function TagField({error, errorMessage, className, name, initialT
 					</button>
 				</div>
 			</div>
-			{errorMessage && <ErrorMessage error={errorMessage} />}
+			{(() => {
+				const message = errors.find(error => error.message)?.message
+				return message ? <ErrorMessage error={message} /> : null
+			})()}
 		</div>
 	)
 }
 
-function Tag({tag, onRemove}: {tag: string; onRemove: (tag: string) => void}) {
+function Tag({tag, onRemove, error}: {tag: string; onRemove: () => void; error: boolean}) {
 	return (
-		<div className='flex gap-8 rounded-full bg-primary-100 px-12 py-8 text-primary-500 dark:bg-primary-600 dark:text-primary-100'>
-			{tag}
-			<button
-				type='button'
-				className='cursor-pointer hover:text-primary-400 dark:hover:text-primary-200'
-				onClick={() => onRemove(tag)}>
-				<FontAwesomeIcon size='sm' icon={faXmark} />
-			</button>
+		<div>
+			<div
+				className={cn(
+					'flex gap-8 rounded-full bg-primary-100 px-12 py-8 text-primary-500 dark:bg-primary-600 dark:text-primary-100',
+					{
+						'bg-error-200 text-error-600 ring ring-error-600 dark:bg-error-600 dark:text-error-100 dark:ring-transparent':
+							error,
+					},
+				)}>
+				{tag}
+				<button
+					type='button'
+					className={cn('cursor-pointer hover:text-primary-400 dark:hover:text-primary-200', {
+						'hover:text-error-500 dark:hover:text-error-200': error,
+					})}
+					onClick={onRemove}>
+					<FontAwesomeIcon size='sm' icon={faXmark} />
+				</button>
+			</div>
 		</div>
 	)
 }
