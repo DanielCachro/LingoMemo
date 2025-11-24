a19858dbaf7406132ac6fc720d1d4e180202aa70
DanielCachro <danielcachro@gmail.com>
2025-11-24 19:04:21 +0100

    fix: ensure clicking input container triggers focus
    - resolve issue where clicking the background area of the component failed to focus the input
    - improve usability by ensuring the entire input container is interactive

--- components/Form/Input.tsx
+++ components/Form/Input.tsx
@@ -10,7 +10,7 @@ interface Props extends InputProps {
 
 export default function Input({error, errorMessage, className, ...props}: Props) {
 	return (
-		<div>
+		<div className='w-full'>
 			<HeadlessInput
 				className={cn(
 					'w-full rounded-sm border-2 border-background-300 bg-background-50 px-16 py-16 text-base placeholder-background-400 placeholder:font-medium focus-within:border-background-400 focus:outline-none dark:border-background-700 dark:bg-background-900 dark:focus-within:border-background-600 [&::-webkit-datetime-edit]:font-medium',
