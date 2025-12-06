4cceb8d109f733863e70a0e7f045ed61eb2b92db
DanielCachro <danielcachro@gmail.com>
2025-12-06 17:51:19 +0100

    feat(study): comment out unused buttons in Flashcard.tsx
    - disable action buttons that are not currently in use to clean up the UI

--- app/(protected)/study/_components/Flashcard.tsx
+++ app/(protected)/study/_components/Flashcard.tsx
@@ -1,21 +1,19 @@
 'use client'
 import type {Flashcard} from '@/types/study'
-import {faMessage, faMinusCircle} from '@fortawesome/free-solid-svg-icons'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 
 export default function Flashcard({flashcard}: {flashcard: Flashcard}) {
-	const buttonStyle = 'hover:cursor-pointer'
+	// const buttonStyle = 'hover:cursor-pointer'
 
 	return (
 		<div className='flex aspect-4/3 flex-col items-center rounded-sm bg-primary-500 px-32 text-primary-50 shadow-slab-primary-300 dark:bg-primary-600 dark:text-primary-100'>
-			<div className='space-x-8 self-end pt-16'>
+			{/* <div className='space-x-8 self-end pt-16'>
 				<button className={buttonStyle}>
 					<FontAwesomeIcon size='sm' icon={faMinusCircle} />
 				</button>
 				<button className={buttonStyle}>
 					<FontAwesomeIcon size='sm' icon={faMessage} />
 				</button>
-			</div>
+			</div> */}
 			<div className='flex w-full max-w-384 grow flex-col items-center justify-center gap-24 pt-24 pb-48 text-center'>
 				<p className='w-full wrap-break-word'>{flashcard.question}</p>
 				<p className='max-h-128 w-full justify-self-center overflow-y-auto rounded-sm bg-primary-400 px-24 py-16 text-sm wrap-break-word text-primary-300 dark:bg-primary-500'>
