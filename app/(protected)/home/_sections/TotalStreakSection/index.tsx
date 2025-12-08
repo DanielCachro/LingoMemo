7ae4c5440eecfe3413c6220cec1a86e1ce3e169a
DanielCachro <danielcachro@gmail.com>
2025-12-08 19:04:39 +0100

    fix: use dynamic IDs in GradientIcon to fix Safari visibility
    - implement `useId` hook to generate unique attributes for SVG filters and gradients
    - resolve duplicate ID conflicts that caused the icon to vanish on Safari
    - ensure consistent rendering when multiple instances of the icon exist

--- app/(protected)/home/_sections/TotalStreakSection/index.tsx
+++ app/(protected)/home/_sections/TotalStreakSection/index.tsx
@@ -8,7 +8,7 @@ export default async function TotalStreak() {
 		<section className='relative flex flex-col items-center justify-center gap-48 pb-48'>
 			<div className='bg-background-0 relative flex size-128 flex-col items-center justify-center rounded-full border-2 border-background-200 bg-background-50 dark:border-background-700 dark:bg-background-800'>
 				<GradientIcon size={60} />
-				<p className='absolute -bottom-24 translate-y-4 bg-gradient-to-b from-background-500 to-background-800 bg-clip-text text-5xl font-black text-transparent dark:from-background-50 dark:to-background-400'>
+				<p className='absolute -bottom-24 translate-y-4 bg-linear-to-b from-background-500 to-background-800 bg-clip-text text-5xl font-black text-transparent dark:from-background-50 dark:to-background-400'>
 					<TotalStreakAnimated />
 				</p>
 			</div>
